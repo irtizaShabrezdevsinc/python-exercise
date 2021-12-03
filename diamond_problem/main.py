d5d95d3744a6d802106aa4c37f03c9de79e77de4
Dev <muhammad.irtiza@devsinc.com>
2021-12-03 18:06:59 +0500

    diamond added

--- diamond_problem/main.py
+++ diamond_problem/main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+class A:
+    def method(self):
+        print("This is method from class A")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class B(A):
+    def method(self):
+        print("This is method from class B")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class C(A):
+    # def method(self):
+    #     print("This is method from class C")
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class D(B, C):
+    def method(self):
+        print("This is method from class D")
+
+
+a = C()
+a.method()
